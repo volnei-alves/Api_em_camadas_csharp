e5d56a59908f13d8e98f2665610e7b963d234e31
Volnei alves <volneialves.f@gmail.com>
2021-08-09 02:55:31 -0300

    Adicionar arquivos de projeto.

---
+++ Infrastructure/Percistencia/DbContextApi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Percistencia
+{
+    public class DbContextApi : DbContext
+    {
+        public DbContextApi(DbContextOptions<DbContextApi> options) : base(options)
+        {
+
+        }
+    }
+}
